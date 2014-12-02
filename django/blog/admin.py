ad4648d4ce00c8fad673bd8f56cbc31b0e506a87
kotrfa <kotrfa@gmail.com>
2014-12-02 13:24:08 +0100

    pridano zobrazovani kategorie u prehledu clanku v admin

--- django/blog/admin.py
+++ django/blog/admin.py
@@ -4,8 +4,12 @@ from blog.models import Blog, Category
 class BlogAdmin(admin.ModelAdmin):
     exclude = ['posted', 'last_mod']
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'posted', 'publish')
+    list_display = ('title', 'posted', 'publish', 'get_category_title')
 
+    def get_category_title(self, obj):
+        return obj.category.title
+    get_category_title.short_description = "Category"
+    get_category_title.admin_order_field = "category__title"
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
