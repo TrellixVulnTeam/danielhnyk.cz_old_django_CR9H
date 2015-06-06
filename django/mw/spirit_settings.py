1ce44a067ca29f46b819b841405d2c74ebf917e3
kotrfa <kotrfa@gmail.com>
2015-06-06 19:28:08 +0200

    mail etc.

--- django/mw/spirit_settings.py
+++ django/mw/spirit_settings.py
@@ -4,3 +4,7 @@ from .settings import *
 SITE_ID = 4 # spirit.danielhnyk.cz
 
 INSTALLED_APPS += ( "djrill",)
+
+MANDRILL_API_KEY = "j2Go0h1D1CM4kdhyYxHh7w"
+EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
+DEFAULT_FROM_EMAIL = "kotrfa@gmail.com"
