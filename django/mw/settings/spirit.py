a1217e988230d4d817a7cefe2a0c2261184983c5
kotrfa <kotrfa@gmail.com>
2015-06-07 11:54:02 +0200

    whoosh index

--- django/mw/settings/spirit.py
+++ django/mw/settings/spirit.py
@@ -10,3 +10,10 @@ ROOT_URLCONF = 'mw.urls_spirit'
 MANDRILL_API_KEY = "j2Go0h1D1CM4kdhyYxHh7w"
 EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 DEFAULT_FROM_EMAIL = "kotrfa@gmail.com"
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+    },
+}
