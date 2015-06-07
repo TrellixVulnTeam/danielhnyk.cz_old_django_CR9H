a38943ac786914949e5171e4d3eb73c2e0b0275e
kotrfa <kotrfa@gmail.com>
2015-06-07 11:43:14 +0200

    templates sttings upgraded to 1.8

--- django/mw/settings/blog.py
+++ django/mw/settings/blog.py
@@ -1,9 +1,5 @@
 from .common import *
 
-SOUTH_MIGRATION_MODULES = {
-        'easy_thumbnails': 'easy_thumbnails.south_migrations',
-    }
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -13,10 +9,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
-#    'django.contrib.comments',
-#    'south',
     'blog',
-#    'django_summernote',
     'easy_thumbnails',
     'projects',
     'blog.templatetags',
@@ -26,14 +19,6 @@ INSTALLED_APPS = (
 
 
 ROOT_URLCONF = 'mw.urls_blog'
-TEMPLATE_CONTEXT_PROCESSORS = (
-  'django.contrib.auth.context_processors.auth',
-  'django.core.context_processors.i18n',
-  'django.core.context_processors.request',
-  'django.core.context_processors.static',
-  'django.core.context_processors.media',
-  'context_processors.extra_context',
-)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,13 +33,26 @@ MIDDLEWARE_CLASSES = (
 
 WSGI_APPLICATION = 'mw.wsgi_blog.application'
 
-# Templates
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.template.context_processors.media',
+                'context_processors.extra_context',
+            ],
+            'template_loaders':[
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ]
+        },
+    },
+]
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-SITE_ID = 3 #www.danielhnyk.cz
+SITE_ID = 3  # www.danielhnyk.cz
